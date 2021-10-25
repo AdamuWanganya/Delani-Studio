760352aa105509db7755676d7ee3dccafa0d3dc6
AdamuWanganya <wanganyaadamu@gmail.com>
2021-10-25 18:43:32 +0300

    Add mailchip API in my form

--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
 console.log('connected');
+$(document).ready(function() {
+    $("#design-icon").click(function() {
+        $("#design-icon").slideDown().hide();
+        $("#design").show();
+    });
+    $("#design").click(function() {
+        $("#design").slideUp();
+        $("#design-icon").slideDown();
+    });
+});
 
 $(document).ready(function() {
-    $("#design-icon,#dev-icon,product-icon").click(function() {
-        $("p").show();
+    $("#dev-icon").click(function() {
+        $("#dev-icon").slideDown().hide();
+        $("#dev").show();
+    });
+    $("#dev").click(function() {
+        $("#dev").slideUp();
+        $("#dev-icon").slideDown();
+    });
+});
+$(document).ready(function() {
+    $("#product-icon").click(function() {
+        $("#product-icon").slideDown().hide();
+        $("#product").show();
+    });
+    $("#product").click(function() {
+        $("#product").slideUp();
+        $("#product-icon").slideDown();
     });
 });
